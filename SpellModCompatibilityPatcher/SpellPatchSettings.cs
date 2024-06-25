4152c4b388b242324d6722077160c3c45e2e6f26
aeqw <qwsdcvghyu89@gmail.com>
2024-06-25 13:22:40 +0300

    Add project files.

---
+++ SpellModCompatibilityPatcher/SpellPatchSettings.cs
@@ -0,0 +1,21 @@
+﻿using Mutagen.Bethesda.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellModCompatibilityPatcher {
+    public class SpellPatchSettings {
+        public List<ModKey> BaseMods = [
+            ModKey.FromNameAndExtension("Skyrim.esm"),
+            ModKey.FromNameAndExtension("Update.esm"),
+            ModKey.FromNameAndExtension("Dawnguard.esm"),
+            ModKey.FromNameAndExtension("HearthFires.esm"),
+            ModKey.FromNameAndExtension("Dragonborn.esm")
+            ];
+        public List<ModKey> PreferredOverrideOrder = [
+
+            ];
+    }
+}
